575dfc7a9617074d0dbef72bcfc7fc315938f37a
Your Name <you@example.com>
2024-08-02 11:56:40 +0400

    updated minishell

--- working/src/minishell.c
+++ working/src/minishell.c
@@ -1,74 +1,51 @@
 #include "../inc/minishell.h"
-
-// void wr_color(const char *color, const char *format, ...)
-// {
-// 	va_list	args;
-
-// 	va_start(args, format);
-// 	printf("%s", color);
-// 	vfprintf(stdout, format, args);
-// 	printf("%s", RESET_COLOR);
-// 	va_end(args);
-// }
-
-void wr_color(const char *color, const char *format, ...)
-{
-	va_list	args;
-	char buffer[1024];
-
-	va_start(args, format);
-	printf("%s", color);
-	vsnprintf(buffer, 1024, format, args);
-	fputs(buffer, stdout);
-	printf("%s", RESET_COLOR);
-	va_end(args);
-}
-
-/**
- * RED
- * GREEN
- * YELLOW
- * BLUE
- * MAGENTA
- * CYAN
-*/
+#include "../inc/struct.h"
 
 int	interactive_mode(void)
 {
-	char		*user_input;
 	char		**command;
-	t_cmdline	command_struct;
+	t_cmdline	cmdline;
 
 	while (1)
 	{
-		user_input = readline("😎minishell-v1$ ");
-		if (!user_input)
+		cmdline.user_input = readline("😎minishell-v1$ ");
+		if (!cmdline.user_input)
 			return (perror("realine error"), 126);
-		wr_color (GREEN, "\tuser_input: {%s}\n", user_input);
-		command = lexer(user_input);
-		initialize_struct(&command_struct);
-		extract_redirections(*command , &command_struct);
-		printf("Input redirect: {%s}\n", command_struct.input_redirect);
-		printf("Output redirect: {%s}\n", command_struct.output_redirect);
-		free(command_struct.input_redirect);	//-----------------------------free function------------------------
-		free(command_struct.output_redirect);	//-----------------------------free function------------------------
-	// 	int i = 0; // debugging while loop
-	// 	while(command[i])
-	// 	{
-	// 		printf("string[%i] = {%s}\n", i, command[i]);
-	// 		i++;
-	// 	}
-	// 	free (user_input);//-----------------------------free function------------------------
-	}
+		command = lexer(cmdline.user_input);
+		initialize_struct(&cmdline);
 
+		extract_redirections(*command , &cmdline);
+		print_debug(&cmdline);
+		free(cmdline.input_redirect);
+		free(cmdline.output_redirect);
+		// int i = -1; // debugging while loop
+		// while(command[++i])
+		// 	printf("string[%i] = {%s}\n", i, command[i]);
+		// free (user_input);
+	}
 	return (0);
 }
 
 int	main(int ac, char **av)
 {
 	if (1 < ac)
-		return (printf("%s: %s: is a file or a directory", av[0], av[1]), 126); /* minishell does not take args */
+		return (printf("%s: %s: is a file or a directory", av[0], av[1]), 126);
 	if (interactive_mode())
 		return (printf ("vegy sed!"), 126); /* returns a random value does not have a meaning */
 	return (0);
 }
+
+/**
+ * --> SIGNALS <--
+ * 0: The command executed successfully.
+ * 1: The shell encountered a syntax error or an error in a startup file (e.g.,
+	~/.bashrc).
+ * 2: The shell encountered an error while reading input (e.g., Ctrl+D was
+ 	pressed).
+ * 3-125: The command failed, and the specific value indicates the type of
+ 	error.
+ * 126: The command was not executable (e.g., it was a directory).
+	minishell does not take args
+ * 127: The command was not found.
+ * 128-255: The command was terminated by a signal (e.g., Ctrl+C).
+*/
