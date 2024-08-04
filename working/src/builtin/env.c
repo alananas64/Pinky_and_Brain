d5e990aea5924c94abf73466fe85d5014fd5f314
alananas <user@mail.com>
2024-08-04 09:28:16 +0400

    added env, echo \builtins

--- working/src/builtin/env.c
+++ working/src/builtin/env.c
@@ -1,28 +1,70 @@
 #include "../../inc/builtin.h"
 
-char	**get_env(void)
+typedef struct s_list
 {
-	char		**env;
-	ssize_t		offset;
-	extern char	**environ;
+	char	*content;
+	struct s_list	*next;
+}	t_list;
 
-	env = environ;
-	offset = -1;
-	while (env[++offset] != NULL)
-		;
-	return (env);
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*newnode;
+
+	newnode = (t_list *)malloc(sizeof(t_list));
+	if (!(newnode))
+		return (NULL);
+	newnode -> content = content;
+	newnode -> next = NULL;
+	return (newnode);
 }
 
-char	*get_path(void)
+t_list	*ft_lstlast(t_list *lst)
 {
-	char	*path;
-	int		uncle;
+	if (!lst)
+		return (NULL);
+	while (lst -> next != NULL)
+	{
+		lst = lst -> next;
+	}
+	return (lst);
+}
 
-	uncle = 126;
-	path = getenv("PATH");
-	if (path != NULL)
-		return (path);
-	else
-		perror("PATH environment variable not set\n");
-	exit (uncle);
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*last;
+
+	if ((lst == NULL) || (new == NULL))
+		return ;
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last -> next = new;
+}
+
+void	print(t_list *root)
+{
+	while (root != NULL)
+	{
+		printf("%s\n", root->content);
+		root = root->next;
+	}
+}
+
+void env_command(char **env)
+{
+	int	i;
+	t_list	*env_key_value;
+
+	i = 0;
+	// env_key_value = NULL; // is it important to set it as null in the begining
+	while (env[i])
+	{
+		ft_lstadd_back(&env_key_value, ft_lstnew(env[i]));
+		i++;
+	}
+	print(env_key_value);
+	return (0);
 }
